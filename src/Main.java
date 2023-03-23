bf90b16dc85dc27f7dd374708bb07057b99394b5
iovigor <iovigor@yahoo.com>
2023-03-23 17:12:02 +0300

    add 2 objects

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,8 @@ public class Main {
         Lecture lec1 = LecUtils.create(1);
         Lecture lec2 = LecUtils.create(2);
         Lecture lec3 = LecUtils.create(3);
-        Lecture lec6 = LecUtils.create(4);
-        Lecture lec6 = LecUtils.create(5);
+        Lecture lec4 = LecUtils.create(4);
+        Lecture lec5 = LecUtils.create(5);
         System.out.println("Lecture count = "+Lecture.getCount());
 
     }
